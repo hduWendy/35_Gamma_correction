5558afb3ae2a543cc72cc2dd0155cc66d85d4c8d
hduWendy <576939544@qq.com>
2020-05-15 14:18:03 +0800

    ok

--- 35_Gamma_correction/35_Gamma_correction.cpp
+++ 35_Gamma_correction/35_Gamma_correction.cpp
@@ -2,10 +2,36 @@
 //
 
 #include "stdafx.h"
-
-
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
 int main()
 {
-    return 0;
-}
+	//读取本地的一张图片，将图片进行gamma矫正，并显示出来！
+	cv::Mat srcMat = imread("C:/Users/lenovo/Desktop/数图图片/face.jpg",0);
 
+	uint8_t b[256] = { 0 };
+	for (int t = 255; t > 0; t--)
+	{
+		float t_0_1 = t / 255.0;
+		b[t] = (uint8_t)255 * pow(t_0_1, 0.5);
+	}
+
+	int height = srcMat.rows; //行数
+	int width = srcMat.cols; //每行元素的总元素数量
+	for (int j = 0; j<height; j++)
+	{
+		for (int i = 0; i<width; i++)
+		{
+			//-----------------开始处理每个像素-----------------
+			uint8_t p = srcMat.at<uint8_t>(j, i);
+			srcMat.at<uint8_t>(j, i) = b[p];
+
+			//-------------结束像素处理------------------------
+		} //单行处理结束
+	}
+	imshow("src", srcMat);
+	//等待用户按键
+	waitKey(0);
+	return 0;
+}
